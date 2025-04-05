e884c04d89b1f8ce0c2d11966ac3a63c5ad3f65e
Tomás Benfica <tomasbenfica@agendeiquadras.com.br>
2025-04-05 18:23:14 -0300

    created TireList component

--- src/hooks/useGetAllTires.ts
+++ src/hooks/useGetAllTires.ts
@@ -3,12 +3,12 @@ import { TireRequestParams } from "../types/types";
 import { getAllTires } from "../queries/tire.queries";
 
 const useGetAllTires = (params?: Partial<TireRequestParams>) => {
-  const { data, isPending, isError } = useQuery({
+  const { data, isPending, isError, error, refetch } = useQuery({
     queryKey: ["tires", params],
     queryFn: () => getAllTires(params ?? {}),
   });
 
-  return { data, isPending, isError };
+  return { data, isPending, isError, error, refetch };
 };
 
 export default useGetAllTires;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
+import TireList from "../components/TireList";
+import useGetAllTires from "../hooks/useGetAllTires";
+
 const Home = () => {
+  const { refetch } = useGetAllTires();
+
   return (
     <div className="h-full flex flex-col gap-y-4 items-center">
       <h1 className="text-blue-300">Prolog</h1>
       <h2>Gestão de frota</h2>
 
-      <button onClick={() => console.log("oi")}>Obter lista de pneus</button>
+      <button onClick={() => refetch()}>Atualizar lista</button>
+
+      <TireList />
     </div>
   );
 };

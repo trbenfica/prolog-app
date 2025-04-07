03c6818a3b85e9f164073574a8e209f9b98319ec
Tomás Benfica <tomasbenfica@agendeiquadras.com.br>
2025-04-07 14:12:14 -0300

    finished e2e tests for tirePage

--- cypress/e2e/tirePage.spec.cy.js
+++ cypress/e2e/tirePage.spec.cy.js
@@ -1,17 +1,17 @@
 describe("Página de detalhes do pneu", () => {
   it("deve carregar e exibir os dados do pneu", () => {
-    cy.intercept("GET", "/api/tires/1", { fixture: "tire.json" }).as("getTire");
+    cy.intercept("GET", `${Cypress.env("apiUrl")}/tires/*`, {
+      fixture: "tire.json",
+    }).as("getTire");
 
     cy.visit("/tires/1");
 
     cy.wait("@getTire");
 
     cy.get('[data-testid="tire-details-table"]').should("exist");
-    cy.contains("ID").should("exist");
-    cy.contains("Serial Number").should("exist");
-    cy.contains("Marca").should("exist");
-
-    cy.contains("MICHELIN").should("exist");
+    cy.contains("139502").should("exist");
+    cy.contains("32141").should("exist");
+    cy.contains("Empurrada").should("exist");
   });
 
   it("deve exibir carregando enquanto espera a resposta", () => {
@@ -24,11 +24,11 @@ describe("Página de detalhes do pneu", () => {
 
     cy.visit("/tires/1");
 
-    cy.get('[data-testid="error-alert"]').should("be.visible");
+    cy.get('[data-testid="loading"]').should("be.visible");
     cy.wait("@getTireSlow");
   });
 
-  it.only("deve exibir erro se a requisição falhar", () => {
+  it("deve exibir erro se a requisição falhar", () => {
     cy.intercept("GET", `${Cypress.env("apiUrl")}/tires/*`, {
       statusCode: 500,
       body: { message: "Erro interno" },
@@ -47,6 +47,6 @@ describe("Página de detalhes do pneu", () => {
 
   it("deve exibir erro se o parâmetro de ID for inválido", () => {
     cy.visit("/tires/invalid-id");
-    cy.contains("error: tireId is invalid").should("exist");
+    cy.get('[data-testid="error-alert"]').should("be.visible");
   });
 });
